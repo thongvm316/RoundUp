cf44ffabf68e1e8619461cc34d8a6de2ca3ba65a
ThongVM <vominhthong1603@gmail.com>
2021-03-25 16:48:02 +0700

    f

---
+++ Home-Main Screen/About-Us/assets/js/index.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
